84dd121083451c386e06d5d8198976b79d43b229
Neo Zulu <nzulu023@student.wethinkcode.co.za>
2024-12-06 10:36:05 +0200

    Payment API works

--- self-storage/payments/.aws-sam/build/GetBillingDetailsFunction/process_payment.py
+++ self-storage/payments/.aws-sam/build/GetBillingDetailsFunction/process_payment.py
@@ -6,14 +6,16 @@ def lambda_handler(event, context):
     payment_info = json.loads(event['body'])
     amount = payment_info.get('amount')
     
-    client_id = os.environ['PAYPAL_CLIENT_ID']
-    secret = os.environ['PAYPAL_SECRET']
+    client_id = "AWiNyGjGGsBkUdK2BIgGEromigu4QFVYW05lKt8aeYXrfLyrnLVBt8Lbad0AaKehWnVUvz5w9Mv95Yh9"
+    secret = "EJUE-kxl1fc1ULsuI9Xpoov3uDMJTYT3MclsiLbK3OAKw5v8iMhXupXsUROYSF0p-sIkKKKEap7G5IZ7"
     
     # Get access token
     auth = (client_id, secret)
     response = requests.post('https://api.sandbox.paypal.com/v1/oauth2/token', 
                              auth=auth, data={'grant_type': 'client_credentials'})
     access_token = response.json().get('access_token')
+
+    print(f"ACCESS TOKEN: {access_token}")
 
     # Create payment request
     payment = {
